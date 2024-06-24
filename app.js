fe219225b2e8eae5005087e580fbbb85a435167c
yuxincxoi <yjyj0774@gmail.com>
2024-06-24 23:07:27 +0900

    getMethod 모듈화

--- app.js
+++ app.js
@@ -1,36 +1,10 @@
 const http = require("http");
-const fs = require("fs");
+const getMethod = require("./components/getMethod")
 
 const server = http.createServer((req, res) => {
   if (req.method === "GET") {
-    if (req.url === "/") {
-      const main = fs.readFileSync("./pages/index.html", "utf8");
-
-      res.statusCode = 200;
-      res.setHeader("content-type", "text/html; chatset=utf-8");
-      res.write(main);
-      res.end();
-    }
-    if (req.url === "/calendar.js") {
-      const script = fs.readFileSync("./components/calendar.js", "utf8");
-
-      res.statusCode = 200;
-      res.setHeader("content-type", "text/javascript; charset=utf-8");
-      res.write(script);
-      res.end();
-    }
-    if (req.url === "/style.css") {
-      const style = fs.readFileSync("./styles/style.css", "utf8");
-
-      res.statusCode = 200;
-      res.setHeader("content-type", "text/css; charset=utf-8");
-      res.write(style);
-      res.end();
-    }
+    getMethod(req, res);
   }
 });
 
-const PORT = 3000;
-server.listen(() => {
-  console.log(`http://localhost:${PORT}`);
-});
+server.listen(3000);
---
+++ components/getMethod.js
@@ -0,0 +1,20 @@
+const readFile = require("./readFile")
+const mimeType = require("./mimeType")
+const errMsg = require("./errMsg")
+
+const getMethod = (req, res) => {
+  if (req.url === "/") {
+    readFile("./pages/index.html", mimeType.html, res);
+  }
+  else if (req.url === "/calendar.js") {
+    readFile("./components/calendar.js", mimeType.js, res);
+  }
+  else if (req.url === "/style.css") {
+    readFile("./styles/style.css", mimeType.css, res);
+  } else {
+    res.writeHead(404, { "Content-Type": mimeType.text });
+    res.end(errMsg[404]);
+  }
+}
+
+module.exports = getMethod;
