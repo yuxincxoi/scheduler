17f3ac57c782888622e44899ca4743a538f17496
yuxincxoi <yjyj0774@gmail.com>
2024-07-02 17:26:32 +0900

    달력 division 모듈화 안하기로함

--- components/getMethod.js
+++ components/getMethod.js
@@ -8,9 +8,9 @@ const getMethod = (req, res) => {
   }
   else if (req.url === "/calendar.js") {
     readFile("./components/calendar.js", mimeType.js, res);
-  }else if (req.url === "/sunToSat.js") {
-    readFile("./components/sunToSat.js", mimeType.js, res);}
-  else if (req.url === "/style.css") {
+  } else if (req.url === "/sunToSat.js") {
+    readFile("./components/sunToSat.js", mimeType.js, res);
+  } else if (req.url === "/style.css") {
     readFile("./styles/style.css", mimeType.css, res);
   } else {
     res.writeHead(404, { "Content-Type": mimeType.text });
