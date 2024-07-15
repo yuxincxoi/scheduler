c195d2987aadc39e41a465cdafd9375ac2bf2bf7
yuxincxoi <yjyj0774@gmail.com>
2024-07-15 21:07:19 +0900

    html 구조 변경

--- components/modal.js
+++ components/modal.js
@@ -1,4 +1,4 @@
-const detail = document.getElementById("detail");
+const modal = document.getElementById("modal");
 const createBtn = document.getElementById("createBtn");
 const scheduleContainer = document.getElementById("scheduleContainer");
 const timeBox = document.getElementById("timeBox");
@@ -18,11 +18,11 @@ for (i = 0; i < 24; i++) {
 
 // opne & close 이벤트
 createBtn.addEventListener("click", () => {
-  if (detail.style.height === "50px") {
-    detail.style.height = "90vh";
+  if (modal.style.height === "50px") {
+    modal.style.height = "90vh";
     scheduleContainer.style.display = "flex";
   } else {
-    detail.style.height = "50px";
+    modal.style.height = "50px";
     scheduleContainer.style.display = "none";
   }
 });
