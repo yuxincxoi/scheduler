423c22d1027c996104eb9893e303a73e6d474dd3
최유진 <yjyj0774@gmail.com>
2024-07-26 21:11:40 +0900

    json 파일 읽은 후 배열에 넣기

--- components/makeHtml.js
+++ components/makeHtml.js
@@ -1,3 +1,22 @@
-const makeHtml = (req, res) => {};
+const fs = require("fs");
+
+const makeHtml = (req, res) => {
+  let fileData = [];
+  fs.readdir("./jsonData", (err, dir) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    dir.forEach((value) => {
+      fs.readFile(`./jsonData/${value}`, (err, data) => {
+        if (err) {
+          console.error(err);
+        }
+        // * 읽은 JSON 파일명을 fileData에 넣기
+        fileData.push(`${value.replace(`.json`, "")}:${data}`);
+      });
+    });
+  });
+};
 
 module.exports = makeHtml;
