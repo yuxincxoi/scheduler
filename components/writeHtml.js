1ae6e4b90dcca1e27d3ba1a85a2abff4915d40df
최유진 <yjyj0774@gmail.com>
2024-07-30 23:41:32 +0900

    최종 Html 생성

--- components/writeHtml.js
+++ components/writeHtml.js
@@ -23,7 +23,50 @@ const writeHtml = async () => {
     .join("");
 
   const submitHTML = `
-    <div>${data}</div>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css" />
+    </head>
+    <body>
+    <div id="root">
+    <div id="detail">
+    <div id="createBtn"></div>
+    <div id="scheduleContainer">
+    <div id="contents">
+    <div id="timeBox"></div>
+    <div id="timeLine">${data}</div>
+    </div>
+    <form id="inputBox" action="submit" method="post">
+    <div>
+    <!-- <label for="title">일정</label> -->
+    <input id="title" type="text" name="title" placeholder="일정" />
+    </div>
+    <div>
+    <!-- <label for="time">시간</label> -->
+    <input id="time" type="time" name="time" placeholder="시간" />
+    </div>
+    <div>
+    <!-- <label for="place">장소</label> -->
+    <input id="place" type="text" name="place" placeholder="장소" />
+    </div>
+    <div>
+    <!-- <label for="memo">메모</label> -->
+    <input id="memo" type="text" name="memo" placeholder="메모" />
+    </div>
+    <div>
+    <button id="saveBtn" type="submit">Save</button>
+    </div>
+    </form>
+    </div>
+    </div>
+    </div>
+    <script src="script.js"></script>
+    </body>
+    </html>
   `;
   return submitHTML;
 };
