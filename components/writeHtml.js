1ed8a31230bc23e9de22ec529ed586e8191ff2bf
최유진 <yjyj0774@gmail.com>
2024-08-01 00:02:55 +0900

    생성되는 html 코드 수정

--- components/writeHtml.js
+++ components/writeHtml.js
@@ -24,49 +24,62 @@ const writeHtml = async () => {
 
   const submitHTML = `
     <!DOCTYPE html>
-    <html lang="en">
-    <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css" />
-    </head>
-    <body>
-    <div id="root">
-    <div id="detail">
-    <div id="createBtn"></div>
-    <div id="scheduleContainer">
-    <div id="contents">
-    <div id="timeBox"></div>
-    <div id="timeLine">${data}</div>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Calendar</title>
+  <link rel="stylesheet" href="style.css"></link>
+</head>
+<body>
+  <div id="root">
+    <div id="headContainer">
+      <div id="yearContainer"></div>
+      <div id="monthContainer"></div>
     </div>
-    <form id="inputBox" action="submit" method="post">
-    <div>
-    <!-- <label for="title">일정</label> -->
-    <input id="title" type="text" name="title" placeholder="일정" />
+    <div id="calendar">
+
     </div>
-    <div>
-    <!-- <label for="time">시간</label> -->
-    <input id="time" type="time" name="time" placeholder="시간" />
+    <div id="modal">
+      <div id="createBtn"></div>
+      <div id="scheduleContainer">
+        <div id="contents">
+          <div id="timeContainer">
+            <div id="timeBox"></div>
+            <div id="timeLine">${data}</div>
+          </div>
+          <form id="inputBox" action="submit" method="post">
+            <div>
+              <!-- <label for="title">일정</label> -->
+              <input id="title" type="text" name="title" placeholder="일정" />
+            </div>
+            <div>
+              <!-- <label for="time">시간</label> -->
+              <input id="time" type="time" name="time" placeholder="시간" />
+            </div>
+            <div>
+              <!-- <label for="place">장소</label> -->
+              <input id="place" type="text" name="place" placeholder="장소" />
+            </div>
+            <div>
+              <!-- <label for="memo">메모</label> -->
+              <input id="memo" type="text" name="memo" placeholder="메모" />
+            </div>
+            <div>
+              <button id="saveBtn" type="submit">Save</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
-    <div>
-    <!-- <label for="place">장소</label> -->
-    <input id="place" type="text" name="place" placeholder="장소" />
-    </div>
-    <div>
-    <!-- <label for="memo">메모</label> -->
-    <input id="memo" type="text" name="memo" placeholder="메모" />
-    </div>
-    <div>
-    <button id="saveBtn" type="submit">Save</button>
-    </div>
-    </form>
-    </div>
-    </div>
-    </div>
-    <script src="script.js"></script>
-    </body>
-    </html>
+    <!-- <div id="modalBackground"></div> -->
+    <div id="subBackground"></div>
+
+  </div>
+  <script src="calendar.js" type="module"></script>
+  <script src="modal.js" type="module"></script>
+</body>
+</html>
   `;
   return submitHTML;
 };
