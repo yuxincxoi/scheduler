9a51388a717282bcab87be2915ae9360d30d05b9
최유진 <yjyj0774@gmail.com>
2024-08-04 19:42:41 +0900

    클릭한 날짜의 년,월,일 데이터 POST로 서버에 전송

--- components/calendar.js
+++ components/calendar.js
@@ -60,10 +60,21 @@ for (let i = 1; i < 43; i++) {
     if (event.target.classList.contains("dayBox")) {
       openModal();
 
-      const selectedDate = event.target.textContent;
-      console.log(yearContainer.textContent);
-      console.log(monthContainer.textContent);
-      console.log(selectedDate);
+      const selectedYear = yearContainer.textContent;
+      const selectedMonth = monthContainer.textContent;
+      const selectedDay = event.target.textContent;
+
+      fetch("/submit", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: new URLSearchParams({
+          year: selectedYear,
+          month: selectedMonth,
+          day: selectedDay,
+        }),
+      });
     }
   });
 }
