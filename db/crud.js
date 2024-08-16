014a810a0dc65bf41c037633dcbe33e0c0e01712
최유진 <yjyj0774@gmail.com>
2024-08-16 17:12:38 +0900

    read 함수 생성

--- db/crud.js
+++ db/crud.js
@@ -8,17 +8,19 @@ const createData = async (data) => {
   await connection.query(createQuery, data);
 };
 
+// * read
+const readData = async () => {
+  const connection = await getConnection();
+  const readQuery = "SELECT * FROM scheduler";
+  const [rows] = await connection.query(readQuery);
+  await connection.end();
+  return rows;
+};
+
 const CUDData = async (query, data) => {
   const connection = await getConnection();
   await connection.query(query, data);
   await connection.end();
-};
-
-const readData = async (query) => {
-  const connection = await getConnection();
-  const [rows] = await connection.query(query);
-  await connection.end();
-  return rows;
 };
 
 module.exports = { CUDData, readData };
