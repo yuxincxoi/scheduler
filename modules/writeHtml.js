6626c7085395a4b33cc91902670bf230723f4f35
최유진 <yjyj0774@gmail.com>
2024-08-18 19:56:10 +0900

    json 데이터 읽는 부분 주석 처리

--- modules/writeHtml.js
+++ modules/writeHtml.js
@@ -1,29 +1,29 @@
 const readJson = require("./readJson");
 
 const writeHtml = async () => {
-  const fileData = await readJson();
+  // const fileData = await readJson();
 
   // * fileData 파싱
-  const data = fileData
-    .map((item) => {
-      try {
-        const [title, ...jsonParts] = item.split(":");
-        const jsonString = jsonParts.join(":");
-        const parsedData = JSON.parse(jsonString);
+  // const data = fileData
+  //   .map((item) => {
+  //     try {
+  //       const [title, ...jsonParts] = item.split(":");
+  //       const jsonString = jsonParts.join(":");
+  //       const parsedData = JSON.parse(jsonString);
 
-        return `<div id="eachSchedule">
-                  <div id="eachScheduleHeader">
-                    <h3>${parsedData.time}</h3>
-                    <h3 id="title">${parsedData.title}</h3>
-                  </div>
-                    <p>${parsedData.place}</p>
-                </div>`;
-      } catch (err) {
-        console.error("Error parsing JSON for item:", item, err);
-        return "";
-      }
-    })
-    .join("");
+  //       return `<div id="eachSchedule">
+  //                 <div id="eachScheduleHeader">
+  //                   <h3>${parsedData.time}</h3>
+  //                   <h3 id="title">${parsedData.title}</h3>
+  //                 </div>
+  //                   <p>${parsedData.place}</p>
+  //               </div>`;
+  //     } catch (err) {
+  //       console.error("Error parsing JSON for item:", item, err);
+  //       return "";
+  //     }
+  //   })
+  //   .join("");
 
   const submitHTML = `
     <!DOCTYPE html>
