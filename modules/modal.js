39610f7d1c23163b2638a5b427648c5f40712d17
최유진 <yjyj0774@gmail.com>
2024-08-26 21:52:34 +0900

    사용하지 않는 코드 삭제

--- modules/modal.js
+++ modules/modal.js
@@ -3,20 +3,6 @@ import closeModal from "./closeModal.js";
 const modal = document.getElementById("modal");
 const createBtn = document.getElementById("createBtn");
 const scheduleContainer = document.getElementById("scheduleContainer");
-const timeBox = document.getElementById("timeBox");
-const inputBox = document.getElementById("inputBox");
-const title = document.getElementById("title");
-const time = document.getElementById("time");
-const place = document.getElementById("place");
-const memo = document.getElementById("memo");
-const saveBtn = document.getElementById("saveBtn");
-
-// 24시간 표시
-// for (let i = 0; i < 24; i++) {
-//   const times = document.createElement("div");
-//   timeBox.append(times);
-//   times.innerText = i + 1;
-// }
 
 // opne & close 이벤트
 createBtn.addEventListener("click", () => {
